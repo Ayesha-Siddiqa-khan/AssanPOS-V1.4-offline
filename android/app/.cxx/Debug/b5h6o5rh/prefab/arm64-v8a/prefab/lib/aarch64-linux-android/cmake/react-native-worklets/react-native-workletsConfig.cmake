f70ed6ea78c63519a695337e80a68f8973f83ff4
Ayesha <proeditorpakistanifeeling@gmail.com>
2025-12-21 14:31:22 +0500

    final

--- android/app/.cxx/Debug/b5h6o5rh/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-worklets::worklets)
-add_library(react-native-worklets::worklets SHARED IMPORTED)
-set_target_properties(react-native-worklets::worklets PROPERTIES
-    IMPORTED_LOCATION "E:/Mobile apps projects/AssanPOS-V1.3/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/1b4y6a3q/obj/arm64-v8a/libworklets.so"
-    INTERFACE_INCLUDE_DIRECTORIES "E:/Mobile apps projects/AssanPOS-V1.3/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/b5h6o5rh/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Rising/.gradle/caches/8.14.3/transforms/1bee6ccf3fffea42dc5eddb5cd440a29/transformed/hermes-android-0.81.5-debug/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Rising/.gradle/caches/8.14.3/transforms/1bee6ccf3fffea42dc5eddb5cd440a29/transformed/hermes-android-0.81.5-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
